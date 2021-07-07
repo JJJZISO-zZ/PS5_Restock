4801fc5284992849bcae8687467b4fcac21cdfd7
JJJZISO <55074198+JJJZISO-zZ@users.noreply.github.com>
2021-07-07 18:28:25 -0400

    Archive commit

--- send_mail.py
+++ send_mail.py
@@ -2,9 +2,9 @@ import requests
 
 def send_mail(rec, txt):
     return requests.post(
-        "https://api.mailgun.net/v3/mail.jjjziso.net/messages",
-        auth=("api", "4f48cac49ffe61bcad7ba98edefa7748-074fa10c-dae505fa"),
-        data={"from": "PS5_Monitor <mailgun@mail.jjjziso.net>",
+        "API_URL",
+        auth=("api", "YOUR_API_KEYS"),
+        data={"from": "PS5_Monitor <"FROM_MAIL_GUN">",
               "to": rec,
               "subject": "PS5 to Buy",
               "text": txt})
